a8619c791589754aafd47ab9ad5849435993b167
yinweijie.zh <yinweijie@ccb.com>
2015-05-20 08:46:11 +0800

    shareSDK test

--- Client/Android/src/com/hzuhelper/AppContext.java
+++ Client/Android/src/com/hzuhelper/AppContext.java
@@ -11,6 +11,8 @@ import android.net.NetworkInfo;
 import android.view.Display;
 import android.view.WindowManager;
 
+import cn.smssdk.SMSSDK;
+
 import com.hzuhelper.tools.StringUtils;
 
 /**
@@ -40,6 +42,7 @@ public class AppContext extends Application {
 	public void onCreate() {
 		super.onCreate();
         //注册App异常崩溃处理器
+		SMSSDK.initSDK(this, "<您的appkey>", "<您的appsecret>");
         Thread.setDefaultUncaughtExceptionHandler(AppException.getAppExceptionHandler());        
         initScreenDisplay();
         appContext=this;
