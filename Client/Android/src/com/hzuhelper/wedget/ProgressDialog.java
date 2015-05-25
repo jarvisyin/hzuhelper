2fb6fcc3c220ac35a843ab5e27c66b461ce691cf
jarvisyin <ywj-1991@qq.com>
2015-05-25 21:30:55 +0800

    modified

--- Client/Android/src/com/hzuhelper/wedget/ProgressDialog.java
+++ Client/Android/src/com/hzuhelper/wedget/ProgressDialog.java
@@ -1,13 +1,15 @@
 package com.hzuhelper.wedget;
 
+import com.hzuhelper.R;
+
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 
-public class ProgressDialog extends Dialog {
+public class ProgressDialog extends Dialog{
 
     public ProgressDialog(Context context){
-        super(context, R.style.progress_dialog);
+        super(context, R.style.ProgressDialog);
         init();
     }
 
